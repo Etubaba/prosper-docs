522d469a3058bea561ac4753e9f8e6398ea20264
Etumnu Prosper <johnikechukwu94@gmail.com>
2023-08-10 16:55:42 +0100

    deployment feat:

--- docs-editor/constant/index.ts
+++ docs-editor/constant/index.ts
@@ -1,3 +1,3 @@
 const envUrl = "prosper-docs-vyrc.vercel.app";
-export const BASE_URL = `http://${envUrl}/`;
+export const BASE_URL = `https://${envUrl}/api/v1`;
 export const SOCKET_BASE_URL = `ws://${envUrl}/`;
